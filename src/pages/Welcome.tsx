24ee6fe04e42e0e0684977722e794f0d22580fce
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-17 16:23:17 -0300

    feat: added icons and fonts

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { SafeAreaView ,View, Text, Image, StyleSheet, TouchableOpacity, Dimensions} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
         <Text style={styles.title}>
           Take care of your plants easily
         </Text>
@@ -26,10 +29,12 @@ export function Welcome() {
           style={styles.button}
           activeOpacity={0.7}
         >
-          <Text style={styles.buttonText}>
-            >
-          </Text>
+            <Feather 
+              name="chevron-right"
+              style={styles.buttonIcon}
+            />
         </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -37,21 +42,28 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   button: {
     backgroundColor: colors.green,
@@ -65,8 +77,8 @@ const styles = StyleSheet.create({
   image: {
     height: Dimensions.get('window').width * 0.7
   },
-  buttonText: {
-    color: colors.white,
-    fontSize: 24
+  buttonIcon: {
+    fontSize: 24,
+    color: colors.white
   }
 })
