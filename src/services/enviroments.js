75c99b9cff0d5dca13c640b12fde16141f6cc44e
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-21 18:58:36 -0300

    chore: added static info

---
+++ src/services/enviroments.js
@@ -0,0 +1,8 @@
+const plantsEnvironments = [
+  {"key": "living_room", "title": "Living Room"},
+  {"key": "bedroom", "title": "Bedroom"},
+  {"key": "kitchen", "title": "Kitchen"},
+  {"key": "bathroom", "title": "Bathroom"}
+]
+
+export default plantsEnvironments;
---
+++ src/services/water_frequencies.js
@@ -0,0 +1,6 @@
+const plantsWaterFrequencies = [
+  {"key": "day", "title": "Everyday"},
+  {"key": "week", "title": "Every week"}
+]
+
+export default plantsWaterFrequencies;
