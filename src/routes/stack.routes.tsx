970f3dcf8b7b18a6314aba8f045ecf9630679c13
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-22 11:07:17 -0300

    refactor: routing

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,9 +7,8 @@ import colors from '../styles/colors';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
-import { PlantSave } from '../pages/PlantSave';
 
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createNativeStackNavigator();
 
@@ -33,12 +32,16 @@ const AppRoutes: React.FC = () => (
       />
       <stackRoutes.Screen 
         name="PlantSelect"
-        component={PlantSelect} 
+        component={AuthRoutes} 
       />
       <stackRoutes.Screen 
         name="PlantSave"
-        component={PlantSave} 
+        component={AuthRoutes} 
       />
+        <stackRoutes.Screen 
+          name="MyPlants"
+          component={AuthRoutes}
+        />
     </stackRoutes.Navigator>
 )
 
