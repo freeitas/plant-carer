ceedc6f145db11f9331ba92c7fa9d283c17b0c12
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-23 14:40:56 -0300

    Notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
 
 import Routes from './src/routes'
+import { PlantProps } from './src/libs/storage';
 
 import {
   useFonts,
@@ -15,6 +17,27 @@ export default function App() {
     Jost_400Regular,
     Jost_600SemiBold
   })
+
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      }
+    )
+
+    return () => subscription.remove();
+
+  },[])
 
   if(!fontsLoaded)
     return <AppLoading />
